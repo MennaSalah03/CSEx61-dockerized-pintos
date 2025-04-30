f9c9800a88a3c83012ad488a59bda13a3b833483
mennasalah <menna.skh2003@gmail.com>
2025-05-01 02:00:47 +0300

    ADD: prior syscall implementation modifications (not yet done)

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -28,9 +28,14 @@ static bool load (const char *cmdline, void (**eip) (void), void **esp);
 tid_t
 process_execute (const char *file_name) 
 {
+  char *exec_name = strchr(*file_name, '/');
   char *fn_copy;
   tid_t tid;
 
+  if (exec_name != NULL)
+  {
+    exec_name++;
+  }
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
@@ -39,7 +44,7 @@ process_execute (const char *file_name)
   strlcpy (fn_copy, file_name, PGSIZE);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (exec_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
@@ -88,6 +93,11 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
+  while (1)
+  {
+    thread_yield();
+  }
+  
   return -1;
 }
 
@@ -437,7 +447,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12;
       else
         palloc_free_page (kpage);
     }
